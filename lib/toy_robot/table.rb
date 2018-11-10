e5df143433c42e516ee391916fe6f849e4292637
pawgrmr <27719504+pawgrmr@users.noreply.github.com>
2018-11-10 11:40:25 +1100

    skeleton + Table class/tests

---
+++ lib/toy_robot/table.rb
@@ -0,0 +1,8 @@
+class Table
+	attr_accessor :width, :height
+	def initialize(width, height)
+		@width = width
+		@height = height
+	end
+
+end
---
+++ spec/table_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+RSpec.describe Table do
+	describe '#initialize' do
+		subject = Table.new(5, 5)
+
+		it "has a width" do
+			expect(subject.width).to eq(5)
+		end
+
+		it "has a height" do
+			expect(subject.height).to eq(5)
+		end
+
+		it 'has customizable height and width' do
+			customizable_object = Table.new(6, 6)
+			expect(customizable_object.height).to eq(6)
+			expect(customizable_object.width).to eq(6)
+		end
+	end
+
+	
+end
